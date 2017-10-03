4af83bc269d48c82a53864f4b0ab5a693d2d5a57
Jacktivity <jh140695@gmail.com>
2017-10-03 22:16:21 +0100

    Mouse Controls
    Mouse click works, box changes colour on click dependant on element
    selected

--- Elements/Assets/Scripts/FollowCursor.cs
+++ Elements/Assets/Scripts/FollowCursor.cs
@@ -10,12 +10,14 @@ public class FollowCursor : MonoBehaviour {
     Texture2D currentTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
-	// Use this for initialization
-	void Start ()
+    public string element;
+    // Use this for initialization
+    void Start ()
     {
         Cursor.visible = true;
         currentTexture = cursorTextureWind;
         Cursor.SetCursor(currentTexture, Vector2.zero, cursorMode);
+        element = "wind";
     }
 	
 	// Update is called once per frame
@@ -24,18 +26,22 @@ public class FollowCursor : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.Q))
         {
             currentTexture = cursorTextureWind;
+            element = "wind";
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             currentTexture = cursorTextureFire;
+            element = "fire";
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
             currentTexture = cursorTextureEarth;
+            element = "earth";
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             currentTexture = cursorTextureWater;
+            element = "water";
         }
         Cursor.SetCursor(currentTexture, Vector2.zero, cursorMode);
     }
---
+++ Elements/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    Vector3 mousePos;
+    string currentElement;
+    int elementSwitch;
+    GameObject princess;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        princess = GameObject.FindGameObjectWithTag("Princess");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentElement = princess.GetComponent<FollowCursor>().element;
+
+	}
+    void OnMouseDown()
+    {
+        //Destroy(gameObject);
+       // Debug.Log(mousePos);
+        switch(currentElement)
+        {
+            case "fire":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+            case "water":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                break;
+            case "wind":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
+                break;
+            case "earth":
+                gameObject.GetComponent<SpriteRenderer>().color = Color.blue + Color.red;
+                break;
+            default:
+                break;
+        }
+    }
+}
