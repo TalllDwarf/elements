b22e255eda1e4c34823d34e3db00492036c1fd1a
Jacktivity <jh140695@gmail.com>
2017-10-07 18:44:32 +0100

    Fixed Quit Menu
    Needed to add object to run quit function from, I'd personally find a replacement object

--- Elements/Assets/Main_Menu/Game_Quit.cs
+++ Elements/Assets/Main_Menu/Game_Quit.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class Game_Quit : MonoBehaviour {
 
     public void quitGame()
     {
-        Debug.Log("Has Quit The Game");
         Application.Quit();
     }
 
--- Elements/Assets/QuitOnClick.cs
+++ Elements/Assets/QuitOnClick.cs
@@ -6,10 +6,11 @@ public class QuitOnClick : MonoBehaviour {
 
     public void Quit()
     {
+        Debug.Log("Clicked");
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPaused = false;
-#else
-        Application.quit();
-#endif
+       UnityEditor.EditorApplication.isPaused = false;
+       #else
+       Application.Quit();
+       #endif
     }
 }
