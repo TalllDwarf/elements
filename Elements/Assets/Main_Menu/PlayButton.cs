f9e779da338c1a8c2c282421067930aec20f6580
Thomas Howe <howethomas98@gmail.com>
2017-10-08 22:40:58 +0100

    Main_Menu_V2.1

--- Elements/Assets/Main_Menu/PlayButton.cs
+++ Elements/Assets/Main_Menu/PlayButton.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class PlayButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Play()
+    {
+        Application.LoadLevel(1);
+    }
 }
