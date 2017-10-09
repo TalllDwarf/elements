1c03ba38bccfba2bb445f70d8607a2316364ac43
Thomas Howe <howethomas98@gmail.com>
2017-10-09 19:51:41 +0100

    UI_V1

---
+++ Elements/Assets/Pause/QuitToMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitToMenu : MonoBehaviour {
+
+    public void QuitToMainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
